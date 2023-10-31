aed9d9d1142290e0835c22c34103ed2ce5663862
pnson <43614413+pns1681999@users.noreply.github.com>
2023-10-31 11:41:34 +0700

    feat: form

--- src/renderer.js
+++ src/renderer.js
@@ -1,5 +1,12 @@
-function onClick() {
-  var executablePath =
-    'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe';
+document.getElementById('exe-form').addEventListener('submit', submitForm);
+
+function submitForm(event) {
+  event.preventDefault(); // Prevent the default form submission behavior
+
+  // Get the input field value
+  var executablePath = document.getElementById('exe-path').value;
+
+  // You can now do something with the input value, for example, display it in an alert
   window.api.runPath(executablePath);
+  // You can also send the value to a server using AJAX or perform any other desired action.
 }
