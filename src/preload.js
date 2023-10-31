2c0e706da4714534e00f0f24a853ab186c949018
pnson <43614413+pns1681999@users.noreply.github.com>
2023-10-31 11:03:54 +0700

    feat: click button

--- src/preload.js
+++ src/preload.js
@@ -1,2 +1,19 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+
+// Get button btn-open-exe
+const btnOpenExe = document.getElementById('btn-open-exe');
+
+btnOpenExe.addEventListener('click', () => {
+  // import child_process for open exe file
+  const { exec } = require('child_process');
+  // open exe file
+  const exeFilePath = '';
+  exec(exeFilePath, (err, data) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log(data);
+  });
+});
